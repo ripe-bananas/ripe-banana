b1450ac2189049c9600d478eb973c0dc58eb719f
allisonbusse <allisonbusse@gmail.com>
2019-09-26 16:35:49 -0700

    finished studio vertical

--- e2e/__tests__/studios.test.js
+++ e2e/__tests__/studios.test.js
@@ -15,17 +15,61 @@ describe('studios api routes', () => {
     }
   };
 
-  it('posts a studio', () => {
+  function postStudio(studio) {
     return request
       .post('/api/studios')
       .send(studio)
       .expect(200)
-      .then(({ body }) => {
-        expect(body).toEqual({
-          ...studio,
-          _id: expect.any(String),
-          __v: 0
-        });
+      .then(({ body }) => body);
+  }
+
+  it('posts a studio', () => {
+    return postStudio(studio).then(pixar => {
+      expect(pixar).toEqual({
+        ...studio,
+        _id: expect.any(String),
+        __v: 0
       });
+    });
+  });
+
+  it('gets all studios', () => {
+    return Promise.all([
+      postStudio(studio),
+      postStudio({ name: 'MGM' }),
+      postStudio({ name: 'A24' })
+    ])
+      .then(() => {
+        return request.get('/api/studios').expect(200);
+      })
+      .then(({ body }) => {
+        expect(body.length).toBe(3);
+      });
+  });
+
+  it('gets a studio by id', () => {
+    return postStudio(studio).then(savedStudio => {
+      return request
+        .get(`/api/studios/${savedStudio._id}`)
+        .expect(200)
+        .then(({ body }) => {
+          expect(body).toMatchInlineSnapshot(
+            {
+              _id: expect.any(String)
+            },
+            `
+            Object {
+              "__v": 0,
+              "_id": Any<String>,
+              "address": Object {
+                "city": "Emeryville",
+                "country": "USA",
+                "state": "CA",
+              },
+              "name": "Pixar",
+            }
+          `);
+        });
+    });
   });
 });
