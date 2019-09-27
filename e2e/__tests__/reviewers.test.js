6b7937e6fe3881842ae732270d3f9f6cebb98632
allisonbusse <allisonbusse@gmail.com>
2019-09-27 15:09:58 -0700

    working on reviewer get by id

--- e2e/__tests__/reviewers.test.js
+++ e2e/__tests__/reviewers.test.js
@@ -1,7 +1,9 @@
 const request = require('../request');
 const db = require('../db');
+const { ObjectId } = require('mongoose').Types;
 
-describe('reviewers api routes', () => {
+
+describe.only('reviewers api routes', () => {
   beforeEach(() => {
     return db.dropCollection('reviewers');
   });
@@ -11,10 +13,32 @@ describe('reviewers api routes', () => {
     company: 'Peace Pies'
   };
 
+  const review = {
+    rating: 5,
+    reviewer: {},
+    review: 'Really good movie!',
+    film: new ObjectId()
+  };
+
+  const reviewTwo = {
+    rating: 2,
+    reviewer: {},
+    review: 'Really really really good movie!',
+    film: new ObjectId()
+  };
+
   function postReviewer(reviewer) {
     return request
       .post('/api/reviewers')
       .send(reviewer)
+      .expect(200)
+      .then(({ body }) => body);
+  }
+
+  function postReview(review) {
+    return request
+      .post('/api/reviews')
+      .send(review)
       .expect(200)
       .then(({ body }) => body);
   }
@@ -47,17 +71,21 @@ describe('reviewers api routes', () => {
 
   it('gets reviewer by id', () => {
     return postReviewer(reviewer)
+      .then(joe => {
+        review.reviewer = joe._id;
+        reviewTwo.reviewer = joe._id;
+        return Promise.all([
+          postReview(review),
+          postReview(reviewTwo)
+        ]);
+      })
       .then((joe) => {
         return request
-          .get(`/api/reviewers/${joe._id}`)
+          .get(`/api/reviewers/${joe[0].reviewer}`)
           .expect(200);
       })
       .then(({ body }) => {
-        expect(body).toEqual({
-          ...reviewer,
-          _id: expect.any(String),
-          __v: 0
-        });
+        expect(body).toEqual('');
       });
 
 
